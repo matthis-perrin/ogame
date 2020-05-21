a968d237449042fdb87643816166ef783a2817bf
Benjamin Meudre <bmeudre@gmail.com>
2020-05-21 22:39:34 +0200

    Updated ghosts

--- extension/src/models/ghost.ts
+++ extension/src/models/ghost.ts
@@ -19,11 +19,11 @@ export const DefaultGhosts: Map<string, string> = new Map();
 // REC
 DefaultGhosts.set('3-1', '5-1');
 DefaultGhosts.set('5-1', '3-2');
-DefaultGhosts.set('3-2', '5-4');
-DefaultGhosts.set('5-4', '3-1');
+DefaultGhosts.set('3-2', '5-2');
+DefaultGhosts.set('5-2', '3-1');
 // BOM
-DefaultGhosts.set('4-1', '5-2');
-DefaultGhosts.set('5-2', '4-2');
+DefaultGhosts.set('4-1', '5-4');
+DefaultGhosts.set('5-4', '4-2');
 DefaultGhosts.set('4-2', '5-3');
 DefaultGhosts.set('5-3', '4-1');
 DefaultGhosts.set('4-3', '5-1');
